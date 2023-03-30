fd5e4a5d9fc0c7b03067a7ad2b63a4281ecf0a70
tammrmakar <tamerboules1@gmail.com>
2023-03-29 21:24:09 -0400

    the color feature have been added

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,15 @@ public class Main {
         car.power = "5000";
         car.color = "Black";
 
-        Engine X =new Engine();
-        X.cylinders="8";
-        X.size="1234cc";
-        X.weight="6453k";
+        Engine X = new Engine();
+        X.cylinders = "8";
+        X.size = "1234cc";
+        X.weight = "6453k";
+
+        color col = new color();
+        col.colorLights = "true";
+        col.colorType = "false";
+        col.colorWindows="true";
+
     }
 }
