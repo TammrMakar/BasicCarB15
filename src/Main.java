2f847857495862b9b89600d77be2b262a26114cd
tammrmakar <tamerboules1@gmail.com>
2023-03-29 21:07:28 -0400

    the lights have been added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        solarSystem solar=new solarSystem();
+        solar.sun="1";
+        solar.moon="2";
+        solar.planets="2";
+
+
+        featureSun Xron=new featureSun();
+        Xron.temperature="21231231231k";
+        Xron.radius="342423423";
+        Xron.color="pink";
+
+
+        lights neon = new lights();
+
+        neon.color="red";
+        neon.size="15cm";
+        neon.intensity="7675lumin";
+
     }
 }
